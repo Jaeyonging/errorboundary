f2fe4e0e5725f778a2b59679fc5a0efa7ed8a939
jaeyonging <wodyd1318@naver.com>
2025-03-13 18:19:48 +0900

    add errorFallback

--- src/component/ErrorFallback.tsx
+++ src/component/ErrorFallback.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 
 export const ErrorFallback = ({
   error,
@@ -8,20 +7,13 @@ export const ErrorFallback = ({
   error: Error;
   resetErrorBoundary: () => void;
 }) => {
-  const statusCode = axios.isAxiosError(error) ? error.response?.status : null;
-
+  const statusCode = (error as any)?.response?.status || null;
   return (
     <div role="alert" className="flex flex-col items-center justify-center w-[100vw] h-[100vh]">
-      {statusCode && (
-        <div className="text-[120px] font-bold text-red-500 mb-8">
-          {statusCode}
-        </div>
-      )}
+      <h1>{statusCode}</h1>
       <pre className="text-[18px] my-4 text-gray-700">{error.message}</pre>
       <div>
-        <p className="text-[24px] font-bold">
-          {error.name}
-        </p>
+        <p className="text-[24px] font-bold">{error.name}</p>
       </div>
       <button
         className="px-4 py-2 bg-blue-500 text-white rounded"
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
-import useFetch from "../hooks/useFetch";
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchComments = async () => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/comments");
+  return response.data;
+};
 
 export const Home = () => {
-  const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/comments');
-  useEffect(()=>{
-    if(data){
-      console.log(data)
-    }
-  },[data])
+  const { data, isLoading, isError, error } = useQuery("comments", fetchComments);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) throw error;
-
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) throw error;
 
   return (
     <div className="flex w-[100vw] flex-col h-[100vh] text-[20px] items-center justify-center">
-      data completed
+      {data.length} comments loaded.
     </div>
   );
 };
